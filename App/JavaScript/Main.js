146a4136d86973519270bd2feaf89be660685a09
Sergey Homyuk <hss@live.ru>
2012-05-22 16:59:51 +0400

    DEV: Fixing link to project source code on GitHub. Wrapping Main into scope.

--- App/JavaScript/Main.js
+++ App/JavaScript/Main.js
@@ -1,20 +1,23 @@
-﻿require.config({
-    paths: {
-        jquery: 'libs/jquery/jquery-1.7.2.min',
-        jqueryplugins: 'libs/jquery/jquery.plugins',
-        knockout: 'libs/knockout/knockout',
-        bootstrap: 'libs/bootstrap/bootstrap.min',
-        text: 'libs/require/text',
-        order: 'libs/require/order'
-    }
+﻿(function (require) {
+    'use strict';
 
-});
+    require.config({
+        paths: {
+            jquery: 'libs/jquery/jquery-1.7.2.min',
+            jqueryplugins: 'libs/jquery/jquery.plugins',
+            knockout: 'libs/knockout/knockout',
+            bootstrap: 'libs/bootstrap/bootstrap.min',
+            text: 'libs/require/text',
+            order: 'libs/require/order'
+        }
 
-require(['jquery', 'CommitFormatter'], function ($, CommitFormatter) {
-    $(function () {
-        var app = new CommitFormatter();
-        app.init();
     });
-});
 
+    require(['jquery', 'CommitFormatter'], function ($, CommitFormatter) {
+        $(function () {
+            var app = new CommitFormatter();
+            app.init();
+        });
+    });
 
+} (require));
